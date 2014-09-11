4803bbcc6fd14a8773645d5b6a9fc0dea3241445
Sean McGary <sean@seanmcgary.com>
2014-09-10 17:54:23 -0700

    creating express middleware

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
 	Manager: require('./lib/manager'),
 	Store: require('./lib/store'),
 	Session: require('./lib/session'),
-	RedisStore: require('./lib/redisStore')
+	RedisStore: require('./lib/redisStore'),
+	Middleware: require('./lib/express')
 };
